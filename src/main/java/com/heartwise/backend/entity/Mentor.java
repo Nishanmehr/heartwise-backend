6d0254b79ba9ac379e0a5c036b65c64d47a36ab5
nishant mehra <nishantmehra172@gmail.com>
2026-04-23 15:46:48 +0530

    Fix Mentor entity - remove conflicting methods

--- src/main/java/com/heartwise/backend/entity/Mentor.java
+++ src/main/java/com/heartwise/backend/entity/Mentor.java
@@ -12,15 +12,15 @@ public class Mentor {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
-    private String firstName;
-    private String lastName;
-    private String email;
-    private String password;
-    private String specialty;
-    private String skills;
-    private String languages;
-    private String gender;
-    private String dob;
+    private String  firstName;
+    private String  lastName;
+    private String  email;
+    private String  password;
+    private String  specialty;
+    private String  skills;
+    private String  languages;
+    private String  gender;
+    private String  dob;
     private Integer experience = 0;
     private Double  price      = 500.0;
     private Double  rating     = 5.0;
@@ -33,13 +33,5 @@ public class Mentor {
 
     public String getName() {
         return (firstName != null ? firstName : "") + " " + (lastName != null ? lastName : "");
-    }
-
-    public String getPrice() {
-        return price != null ? "₹" + price.intValue() : "₹500";
-    }
-
-    public String getExperience() {
-        return experience != null ? experience + " yrs" : "0 yrs";
     }
 }
